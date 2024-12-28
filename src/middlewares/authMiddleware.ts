5875f60d146ae37f0da1f6fe0d744c098482f52f
NarayanaGudiwada <narayanagudiwada@gmail.com>
2024-12-28 22:03:38 +0530

    minor changes

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -6,7 +6,7 @@ export const auth = async (req: Request, res: Response, next: NextFunction): Pro
     try {
         const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
         if (!token) {
-            res.status(401).send({ error: 'Unauthorized' });
+            res.status(401).send({ message: 'Unauthorized' });
         } else {
             const decodedToken = decodeToken(token);
             const userId = decodedToken.userId;
